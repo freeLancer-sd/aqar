ce7547f1ca3a0d28a77186dcf0b9da2bcbf5241e
Tayeb-Ali <Mr.elteb@gmail.com>
2021-01-18 08:18:37 +0000

    fix advs show fields

--- resources/views/advs/show_fields.blade.php
+++ resources/views/advs/show_fields.blade.php
@@ -4,70 +4,10 @@
     <p>{{ $adv->title }}</p>
 </div>
 
-<!-- Address Field -->
-<div class="form-group">
-    {!! Form::label('address', __('models/properties.fields.address').':') !!}
-    <p>{{ $adv->address }}</p>
-</div>
-
-<!-- Lat Field -->
-<div class="form-group">
-    {!! Form::label('lat', __('models/properties.fields.lat').':') !!}
-    <p>{{ $adv->lat }}</p>
-</div>
-
-<!-- Lng Field -->
-<div class="form-group">
-    {!! Form::label('lng', __('models/properties.fields.lng').':') !!}
-    <p>{{ $adv->lng }}</p>
-</div>
-
 <!-- Status Field -->
 <div class="form-group">
     {!! Form::label('status', __('models/properties.fields.status').':') !!}
     <p>{{ $adv->status }}</p>
-</div>
-
-<!-- Room Number Field -->
-<div class="form-group">
-    {!! Form::label('room_number', __('models/properties.fields.room_number').':') !!}
-    <p>{{ $adv->room_number }}</p>
-</div>
-
-<!-- Bath Number Field -->
-<div class="form-group">
-    {!! Form::label('bath_number', __('models/properties.fields.bath_number').':') !!}
-    <p>{{ $adv->bath_number }}</p>
-</div>
-
-<!-- Hall Number Field -->
-<div class="form-group">
-    {!! Form::label('hall_number', __('models/properties.fields.hall_number').':') !!}
-    <p>{{ $adv->hall_number }}</p>
-</div>
-
-<!-- Property Age Field -->
-<div class="form-group">
-    {!! Form::label('property_age', __('models/properties.fields.property_age').':') !!}
-    <p>{{ $adv->property_age }}</p>
-</div>
-
-<!-- Air Conditioner Field -->
-<div class="form-group">
-    {!! Form::label('air_conditioner', __('models/properties.fields.air_conditioner').':') !!}
-    <p>{{ $adv->air_conditioner }}</p>
-</div>
-
-<!-- Space Field -->
-<div class="form-group">
-    {!! Form::label('space', __('models/properties.fields.space').':') !!}
-    <p>{{ $adv->space }}</p>
-</div>
-
-<!-- Price Field -->
-<div class="form-group">
-    {!! Form::label('price', __('models/properties.fields.price').':') !!}
-    <p>{{ $adv->price }}</p>
 </div>
 
 <!-- Note Field -->
@@ -76,149 +16,6 @@
     <p>{{ $adv->note }}</p>
 </div>
 
-<!-- Land Number Field -->
-<div class="form-group">
-    {!! Form::label('land_number', __('models/properties.fields.land_number').':') !!}
-    <p>{{ $adv->land_number }}</p>
-</div>
-
-<!-- Destination Field -->
-<div class="form-group">
-    {!! Form::label('destination', __('models/properties.fields.destination').':') !!}
-    <p>{{ $adv->destination }}</p>
-</div>
-
-<!-- Advertiser Status Field -->
-<div class="form-group">
-    {!! Form::label('advertiser_status', __('models/properties.fields.advertiser_status').':') !!}
-    <p>{{ $adv->advertiser_status }}</p>
-</div>
-
-<!-- Floor Field -->
-<div class="form-group">
-    {!! Form::label('floor', __('models/properties.fields.floor').':') !!}
-    <p>{{ $adv->floor }}</p>
-</div>
-
-<!-- The Number Stores Field -->
-<div class="form-group">
-    {!! Form::label('the_number_stores', __('models/properties.fields.the_number_stores').':') !!}
-    <p>{{ $adv->the_number_stores }}</p>
-</div>
-
-<!-- The Number Apartments Field -->
-<div class="form-group">
-    {!! Form::label('the_number_apartments', __('models/properties.fields.the_number_apartments').':') !!}
-    <p>{{ $adv->the_number_apartments }}</p>
-</div>
-
-<!-- Street Area Field -->
-<div class="form-group">
-    {!! Form::label('street_area', __('models/properties.fields.street_area').':') !!}
-    <p>{{ $adv->street_area }}</p>
-</div>
-
-<!-- The Purpose Field -->
-<div class="form-group">
-    {!! Form::label('the_purpose', __('models/properties.fields.the_purpose').':') !!}
-    <p>{{ $adv->the_purpose }}</p>
-</div>
-
-<!-- Wells Field -->
-<div class="form-group">
-    {!! Form::label('wells', __('models/properties.fields.wells').':') !!}
-    <p>{{ $adv->wells }}</p>
-</div>
-
-<!-- Rental Type Field -->
-<div class="form-group">
-    {!! Form::label('rental_type', __('models/properties.fields.rental_type').':') !!}
-    <p>{{ $adv->rental_type }}</p>
-</div>
-
-<!-- Rental Data Field -->
-<div class="form-group">
-    {!! Form::label('rental_data', __('models/properties.fields.rental_data').':') !!}
-    <p>{{ $adv->rental_data }}</p>
-</div>
-
-<!-- Property Type Field -->
-<div class="form-group">
-    {!! Form::label('property_type', __('models/properties.fields.property_type').':') !!}
-    <p>{{ $adv->property_type }}</p>
-</div>
-
-<!-- Car Entrance Field -->
-<div class="form-group">
-    {!! Form::label('car_entrance', __('models/properties.fields.car_entrance').':') !!}
-    <p>{{ $adv->car_entrance }}</p>
-</div>
-
-<!-- Deluxe Field -->
-<div class="form-group">
-    {!! Form::label('deluxe', __('models/properties.fields.deluxe').':') !!}
-    <p>{{ $adv->deluxe }}</p>
-</div>
-
-<!-- Kitchen Field -->
-<div class="form-group">
-    {!! Form::label('kitchen', __('models/properties.fields.kitchen').':') !!}
-    <p>{{ $adv->kitchen }}</p>
-</div>
-
-<!-- Swimming Pool Field -->
-<div class="form-group">
-    {!! Form::label('swimming_pool', __('models/properties.fields.swimming_pool').':') !!}
-    <p>{{ $adv->swimming_pool }}</p>
-</div>
-
-<!-- Driver Room Field -->
-<div class="form-group">
-    {!! Form::label('driver_room', __('models/properties.fields.driver_room').':') !!}
-    <p>{{ $adv->driver_room }}</p>
-</div>
-
-<!-- Maids Room Field -->
-<div class="form-group">
-    {!! Form::label('maids_room', __('models/properties.fields.maids_room').':') !!}
-    <p>{{ $adv->maids_room }}</p>
-</div>
-
-<!-- Elevator Field -->
-<div class="form-group">
-    {!! Form::label('elevator', __('models/properties.fields.elevator').':') !!}
-    <p>{{ $adv->elevator }}</p>
-</div>
-
-<!-- Furnished Field -->
-<div class="form-group">
-    {!! Form::label('furnished', __('models/properties.fields.furnished').':') !!}
-    <p>{{ $adv->furnished }}</p>
-</div>
-
-<!-- Cellar Field -->
-<div class="form-group">
-    {!! Form::label('cellar', __('models/properties.fields.cellar').':') !!}
-    <p>{{ $adv->cellar }}</p>
-</div>
-
-<!-- Courtyard Field -->
-<div class="form-group">
-    {!! Form::label('courtyard', __('models/properties.fields.courtyard').':') !!}
-    <p>{{ $adv->courtyard }}</p>
-</div>
-
-<!-- Extension Field -->
-<div class="form-group">
-    {!! Form::label('extension', __('models/properties.fields.extension').':') !!}
-    <p>{{ $adv->extension }}</p>
-</div>
-
-<!-- Property Categorie Id Field -->
-<div class="form-group">
-    {!! Form::label('property_categorie_id', __('models/properties.fields.property_categorie_id').':') !!}
-    <p>{{ $adv->property_categorie_id }}</p>
-</div>
 
 <!-- User Id Field -->
 <div class="form-group">
@@ -226,21 +23,10 @@
     <p>{{ $adv->user_id }}</p>
 </div>
 
-<!-- City Id Field -->
-<div class="form-group">
-    {!! Form::label('city_id', __('models/properties.fields.city_id').':') !!}
-    <p>{{ $adv->city_id }}</p>
-</div>
-
-<!-- District Id Field -->
-<div class="form-group">
-    {!! Form::label('district_id', __('models/properties.fields.district_id').':') !!}
-    <p>{{ $adv->district_id }}</p>
-</div>
 
 <!-- Status Mobile Field -->
 <div class="form-group">
-    {!! Form::label('status_mobile', __('models/properties.fields.status_mobile').':') !!}
-    <p>{{ $adv->status_mobile }}</p>
+    {!! Form::label('ads_mobile', __('models/advs.fields.ads_mobile').':') !!}
+    <p>{{ $adv->ads_mobile }}</p>
 </div>
 
