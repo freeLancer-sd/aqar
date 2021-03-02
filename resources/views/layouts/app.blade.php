8cdf221c2510bcaad72455838b70d41f3bdc2f8c
Elteyab Hassan <Elteyab.Hassan@gmail.com>
2021-03-02 09:02:30 +0200

    update image fix save proprtey image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -183,7 +183,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 {{--<script src="{{asset('firebase-messaging-sw.js')}}"></script>--}}
+@stack('scripts')
 
 </body>
 </html>
-@stack('scripts')
